a72cfd97ddd0a06054e88f210d0667091e7824e3
RomainSabatier <rsabatier57@gmail.com>
2026-02-07 11:08:32 +0100

    adaptation tests pour passer a 9chiffres pour numero client

--- src/test/java/com/iut/banque/test/modele/TestsClient.java
+++ src/test/java/com/iut/banque/test/modele/TestsClient.java
@@ -13,13 +13,13 @@ public class TestsClient {
 
     @Test
     public void testPossedeComptesADecouvert_ClientSansCompte() throws Exception {
-        Client c = new Client("John", "Doe", "20 rue Bouvier", true, "j.doe1", "password", "1234567890");
+        Client c = new Client("John", "Doe", "20 rue Bouvier", true, "j.doe1", "password", "123456789");
         assertFalse(c.possedeComptesADecouvert());
     }
 
     @Test
     public void testPossedeComptesADecouvert_ClientAvecComptesSansDecouvert() throws Exception {
-        Client c = new Client("John", "Doe", "20 rue Bouvier", true, "j.doe1", "password", "1234567890");
+        Client c = new Client("John", "Doe", "20 rue Bouvier", true, "j.doe1", "password", "123456789");
         c.addAccount(new CompteSansDecouvert("FR1234567890", 42, c));
         c.addAccount(new CompteSansDecouvert("FR1234567891", 0, c));
         assertFalse(c.possedeComptesADecouvert());
@@ -27,14 +27,14 @@ public class TestsClient {
 
     @Test
     public void testPossedeComptesADecouvert_ClientAvecComptesAvecDecouvert() throws Exception {
-        Client c = new Client("John", "Doe", "20 rue Bouvier", true, "j.doe1", "password", "1234567890");
+        Client c = new Client("John", "Doe", "20 rue Bouvier", true, "j.doe1", "password", "123456789");
         c.addAccount(new CompteAvecDecouvert("FR1234567892", -42, 100, c));
         assertTrue(c.possedeComptesADecouvert());
     }
 
     @Test
     public void testGetComptesAvecSoldeNonNul() throws Exception {
-        Client c = new Client("John", "Doe", "20 rue Bouvier", true, "j.doe1", "password", "1234567890");
+        Client c = new Client("John", "Doe", "20 rue Bouvier", true, "j.doe1", "password", "123456789");
         c.addAccount(new CompteAvecDecouvert("FR1234567890", 0, 42, c));
         c.addAccount(new CompteSansDecouvert("FR1234567891", 1, c));
 
